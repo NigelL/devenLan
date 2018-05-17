81f014c5417f9aafc66ef8b54b7a71f878b6d0a7
nigel lim <nigelLoffical@gmail.com>
2018-05-17 17:08:05 +0800

    sort

--- DevenLand/main.cpp
+++ DevenLand/main.cpp
@@ -2,8 +2,25 @@
 
 // true - wall. false - free
 bool grid[40][40];
+int sortInt[] = { 21 , 35 , 20 };
+
+void Sort(int arrayToSort[])
+{
+	for (int i = 0; i < sizeof(arrayToSort) / sizeof(int); i++)
+	{
+		int key = arrayToSort[i];
+		int j = i - 1;
+		while (j >= 0 && key < arrayToSort[j])
+		{
+			arrayToSort[j + 1] = arrayToSort[j];
+			j--;
+		}
+
+		arrayToSort[j + 1] = key;
+	}
+}
 
 int main() {
-
+	Sort(sortInt);
 	return 0;
 }
